ac7596384799048bc9a0e92110303beeed80cbf1
Timotei GERANTE <79323916+T1masv@users.noreply.github.com>
2025-03-09 20:00:39 +0100

    Update SubscriptionController.java

--- src/main/java/com/example/subsavvy/Controller/SubscriptionController.java
+++ src/main/java/com/example/subsavvy/Controller/SubscriptionController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/subscriptions")
 @RequiredArgsConstructor
 public class SubscriptionController {
 
